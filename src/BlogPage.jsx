203d9011b2b27b76773bd7afc3c5cd5ddbd185f1
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-09-14 19:06:16 -0300

    Uso de Outlet y NestedRoutes

--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { blogData } from "./blogData";
 
 function BlogPage() {
   return (
     <>
-      <h1>Blog Page</h1>
+      <h1>Blog</h1>
+
+      <Outlet />
+
       <ul>
         {blogData.map((post) => (
           <BlogLink key={post.slug} post={post} />
