ecaa898ac5d17f1e0a5869f211745a6dfe382b06
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-09-14 18:49:20 -0300

    corregido warning elementos unicos menu, agregado key

--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -8,7 +8,7 @@ function BlogPage() {
       <h1>Blog Page</h1>
       <ul>
         {blogData.map((post) => (
-          <BlogLink post={post} />
+          <BlogLink key={post.slug} post={post} />
         ))}
       </ul>
     </>
