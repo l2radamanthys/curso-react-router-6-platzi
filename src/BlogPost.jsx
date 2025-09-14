b2b386acd9b49cc34254af33a64c887ce0c15f32
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-09-14 18:55:10 -0300

    react hook useNavigate

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogData } from "./blogData";
 
 function BlogPost() {
+  const navigate = useNavigate();
   const { slug } = useParams();
   const blogPost = blogData.find((post) => post.slug === slug);
+
+  const returnToBlog = () => {
+    // navigate(-1); // -1 go to previos page similar windows.location(-1)
+    navigate("/blog");
+  };
 
   return (
     <>
       <h2>{blogPost.title}</h2>
+      <button onClick={returnToBlog}>Volver al Blog</button>
       <p>Author: {blogPost.author}</p>
       <p>{blogPost.content}</p>
     </>
